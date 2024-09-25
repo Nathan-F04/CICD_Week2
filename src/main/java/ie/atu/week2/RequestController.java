470a1555a7fc407caf6b04a4c27f690809657945
G00413711@atu.ie <G00413711@atu.ie>
2024-09-25 14:42:30 +0100

    Created Request mapping

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -1,12 +1,14 @@
 package ie.atu.week2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/person")
 public class RequestController {
 
-    @GetMapping
+    @GetMapping("/sayHello")
     public String sayHello()
     {
         return "Hi there";
