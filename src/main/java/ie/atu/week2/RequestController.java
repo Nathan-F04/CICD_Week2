353d3b46242ad72f1f10066280e3802656b3c16b
G00413711@atu.ie <G00413711@atu.ie>
2024-09-25 14:52:34 +0100

    Created greetByName with path variable

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.week2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class RequestController {
     public String sayHello()
     {
         return "Hi there";
+    }
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name)
+    {
+        return "Your name is: " + name;
     }
 }
