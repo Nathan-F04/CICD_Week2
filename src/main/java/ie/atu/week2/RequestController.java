d27d08f71b580060d384d269d25099d54b022280
G00413711@atu.ie <G00413711@atu.ie>
2024-09-25 15:34:09 +0100

    Created the calculator method

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/person")
-public class RequestController {
+public class RequestController
+{
 
     @GetMapping("/hello")
     public String sayHello()
@@ -20,5 +21,34 @@ public class RequestController {
     public String details(@RequestParam String name, @RequestParam int age)
     {
         return "Your name is: " + name + " and your age is: " + age;
+    }
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation)
+    {
+        int ans;
+        switch (operation)
+        {
+            case "add":
+                ans= num1+num2;
+                break;
+            case "subtract":
+                ans= (num1-num2);
+                break;
+            case "divide":
+                if(num2==0)
+                {
+                    return "You cannot divide by 0!";
+                }
+                else{
+                    ans= num1/num2;
+                }
+                break;
+            case "multiply":
+                ans= num1*num2;
+                break;
+            default:
+                return "You're in the default statement!";
+        }
+        return Integer.toString(ans);
     }
 }
